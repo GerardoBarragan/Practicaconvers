38990c3132d227438b8c734bd3e09c2b1df26eaa
GerardoBarragan <gera_shark11@hotmail.com>
2013-09-23 22:23:15 -0500

    tarea

---
+++ conversor.php
@@ -0,0 +1,14 @@
+<html>
+<head></head>
+<body>
+<?php
+$dolar=12;
+$peso=5;
+$total;
+echo "5 pesos convertido a costo del dolar igual   ";
+
+$total=$dolar*$peso;
+echo "$total";
+?>
+</body>
+</html>
